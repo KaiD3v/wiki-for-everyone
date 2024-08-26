8a0219bb01311e48361bc8fa5a056e6db6b0b3ba
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-08-25 21:07:34 -0300

    add sidebar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Container } from "../components/Container";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Página Inicial</h1>
-    </main>
+    <Container>
+      <h1>teste</h1>
+    </Container>
   );
 }
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,68 @@
+import {
+  FaAudioDescription,
+  FaEye,
+  FaHandFist,
+  FaMicrophone
+} from "react-icons/fa6";
+
+export function Sidebar() {
+  return (
+    <aside className="text-white bg-black relative w-full h-screen max-w-20 transition-all duration-300 sm:max-w-60 md:max-w-96">
+      <header className="hidden sm:flex flex-col m-6 justify-center items-center gap-3">
+        <h1 className="font-extralight sm:text-2xl">WikiForEveryone</h1>
+        <hr className="w-full border-t-2 border-gray-500" />
+      </header>
+      <section className="sm:hidden flex flex-col h-full">
+        <header className="flex flex-col text-center py-4">
+          <h1 className="font-extralight text-start flex flex-col">
+            Wiki <span>For</span> <span>Everyone</span>
+          </h1>
+          <hr className="w-full border-t-2 border-gray-500" />
+        </header>
+
+        <main className="flex-grow flex flex-col justify-center items-center">
+          <ul className="flex flex-col justify-center items-center gap-5">
+            <li className="cursor-pointer">
+              <div className="p-2 bg-gray-700 rounded-sm hover:bg-gray-600 transition-colors">
+                <FaHandFist size={32} />
+              </div>
+            </li>
+            <li className="cursor-pointer">
+              <div className="p-2 bg-gray-700 rounded-sm hover:bg-gray-600 transition-colors">
+                <FaMicrophone size={32} />
+              </div>
+            </li>
+            <li className="cursor-pointer">
+              <div className="p-2 bg-gray-700 rounded-sm hover:bg-gray-600 transition-colors">
+                <FaEye size={32} />
+              </div>
+            </li>
+            <li className="cursor-pointer">
+              <div className="p-2 bg-gray-700 rounded-sm hover:bg-gray-600 transition-colors">
+                <FaAudioDescription size={32} />
+              </div>
+            </li>
+          </ul>
+        </main>
+      </section>
+
+      <section className="hidden sm:flex flex-col m-6 justify-center items-center gap-3">
+        <h1 className="font-extralight sm:text-2xl mr-10">Ferramentas</h1>
+        <ul className="text-lg font-extralight">
+          <li>
+            <button>Libras</button>
+          </li>
+          <li>
+            <button>Leitura em voz alta</button>
+          </li>
+          <li>
+            <button>Modo Daltonismo</button>
+          </li>
+          <li>
+            <button>Descrição de Imagem</button>
+          </li>
+        </ul>
+      </section>
+    </aside>
+  );
+}
