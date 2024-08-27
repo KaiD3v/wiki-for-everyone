32f84f8b7954786dcc43bb2c2a76aabd07f32556
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-08-27 11:15:20 -0300

    get wiki page data

--- src/app/pages/[title]/page.tsx
+++ src/app/pages/[title]/page.tsx
@@ -1,7 +1,31 @@
-export default function WikiPage(){
-    return (
-        <>
-        
-        </>
-    )
+import { PageProps } from "../../../utils/types/PagesTypes";
+
+async function getPageData(title: string) {
+  try {
+    const response = await fetch(
+      `${process.env.WIKI_API_URL}/page/${title}/with_html`
+    );
+
+    return response.json();
+  } catch (error) {
+    console.log("houve um erro ao tentar buscas os dados da página:" + error);
+    return null;
+  }
+}
+
+export default async function WikiPage({
+  params: { title }
+}: {
+  params: { title: string };
+}) {
+  const pageData: PageProps = await getPageData(title);
+
+  console.log(pageData);
+  return (
+    <main>
+      <h1>
+        {decodeURI(title)}
+      </h1>
+    </main>
+  );
 }
